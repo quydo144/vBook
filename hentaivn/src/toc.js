d27987f579668da4f070a708a65a5a8fa23d1ade
ThanhQuy <quydo144@gmail.com>
2022-12-03 22:56:45 +0700

    Add ext hentaivn

---
+++ hentaivn/src/toc.js
@@ -0,0 +1,43 @@
+function execute(url) {
+    let response = fetch(url, {
+        headers: {
+            "referer": "https://hentaivn.in"
+        }
+    });
+    if (response.ok) {
+        let doc = response.html();
+        let isMobile = doc.select(".header-logo").size() !== 0;
+        let data = [];
+        if (isMobile) {
+            let getUrl = doc.html().match(/(list-showchapter-mobile.php.*?)\"/)[1];
+
+            doc = fetch("https://hentaivn.in/" + getUrl).html();
+            let el = doc.select(".episodes a")
+            for (let i = el.size() - 1; i >= 0; i--) {
+                let e = el.get(i);
+                data.push({
+                    name: e.text(),
+                    url: e.attr("href"),
+                    host: "https://hentaivn.in"
+                })
+            }
+        } else {
+            let getUrl = doc.html().match(/(list-showchapter.php.*?)\"/)[1];
+
+            doc = fetch("https://hentaivn.in/" + getUrl).html();
+            let el = doc.select("a")
+            for (let i = el.size() - 1; i >= 0; i--) {
+                let e = el.get(i);
+                data.push({
+                    name: e.text(),
+                    url: e.attr("href"),
+                    host: "https://hentaivn.in"
+                })
+            }
+        }
+
+        return Response.success(data);
+    }
+
+    return null;
+}
