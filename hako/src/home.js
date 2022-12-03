36e2e5f9c6e0636e516f01e8f66f91a615b38e8b
ThanhQuy <quydo144@gmail.com>
2022-12-03 17:21:50 +0700

    Add ext hako and change domain docln.net

---
+++ hako/src/home.js
@@ -0,0 +1,34 @@
+function execute() {
+    return Response.success([
+        {
+            title: "Mới cập nhật",
+            script: "gen.js",
+            input: "https://docln.net/danh-sach?truyendich=1&sangtac=1&convert=1&dangtienhanh=1&tamngung=1&hoanthanh=1&sapxep=capnhat"
+        },
+        {
+            title: "Truyện mới",
+            script: "gen.js",
+            input: "https://docln.net/danh-sach?truyendich=1&sangtac=1&convert=1&dangtienhanh=1&tamngung=1&hoanthanh=1&sapxep=truyenmoi"
+        },
+        {
+            title: "Theo dõi",
+            script: "gen.js",
+            input: "https://docln.net/danh-sach?truyendich=1&sangtac=1&convert=1&dangtienhanh=1&tamngung=1&hoanthanh=1&sapxep=theodoi"
+        },
+        {
+            title: "Top toàn thời gian",
+            script: "gen.js",
+            input: "https://docln.net/danh-sach?truyendich=1&sangtac=1&convert=1&dangtienhanh=1&tamngung=1&hoanthanh=1&sapxep=top"
+        },
+        {
+            title: "Top toàn thời gian isekai",
+            script: "gen.js",
+            input: "https://docln.net/the-loai/isekai?truyendich=1&dangtienhanh=1&tamngung=1&hoanthanh=1&sapxep=top"
+        },
+        {
+            title: "Top toàn thời gian harem",
+            script: "gen.js",
+            input: "https://docln.net/the-loai/harem?truyendich=1&dangtienhanh=1&tamngung=1&hoanthanh=1&sapxep=top"
+        },
+    ]);
+}
--- hentaivnvip/src/home.js
+++ hentaivnvip/src/home.js
@@ -1,7 +1,6 @@
 function execute() {
     return Response.success([
         { title: "Hôm nay", input: "https://hentaivnvip.net/truyen-hot/truyen-hentai-hot-hom-nay", script: "gen.js" },
-        { title: "Hot Tuần", input: "https://hentaivnvip.net/truyen-hot/truyen-hot-tuan", script: "gen.js" },
         { title: "Manhwa", input: "https://hentaivnvip.net/the-loai/manhwa", script: "gen.js" },
         { title: "Adult", input: "https://hentaivnvip.net/the-loai/adult", script: "gen.js" },
         { title: "Oneshot", input: "https://hentaivnvip.net/the-loai/oneshot", script: "gen.js" },
