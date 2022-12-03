851c74b5418b1556fa3fe4426f83217a5591aa56
ThanhQuy <quydo144@gmail.com>
2022-12-03 16:03:33 +0700

    Update url hentaivnvip

--- hentaivnvip/src/home.js
+++ hentaivnvip/src/home.js
@@ -1,8 +1,10 @@
 function execute() {
     return Response.success([
-        {title: "Cập Nhật", input: "https://hentaivnvip.net/truyen-hentai-moi", script: "gen.js"},
-        {title: "Hot Tuần", input: "https://hentaivnvip.net/truyen-hot", script: "gen.js"},
+        {title: "Hôm nay", input: "https://hentaivnvip.net/truyen-hot/truyen-hentai-hot-hom-nay", script: "gen.js"},
+        {title: "Hot Tuần", input: "https://hentaivnvip.net/truyen-hot/truyen-hot-tuan", script: "gen.js"},
         {title: "Manhwa", input:"https://hentaivnvip.net/the-loai/manhwa", script: "gen.js"},
+        {title: "Adult", input:"https://hentaivnvip.net/the-loai/adult", script: "gen.js"},
+        {title: "Oneshot", input:"https://hentaivnvip.net/the-loai/oneshot", script: "gen.js"},
         {title: "Full Color", input: "https://hentaivnvip.net/the-loai/full-color", script: "gen.js"},
         {title: "Không Che", input: "https://hentaivnvip.net/the-loai/khong-che", script: "gen.js"},
     ]);
