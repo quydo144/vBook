2098b852c49b10c1f48b55f4e11d323f3a641a62
ThanhQuy <quydo144@gmail.com>
2022-12-24 13:14:08 +0700

    Add truyenqq

--- sayhentai/src/home.js
+++ sayhentai/src/home.js
@@ -3,7 +3,6 @@ function execute() {
         { title: "Cập Nhật", input: "https://sayhentai.me", script: "gen.js" },
         { title: "Manhwa", input: "https://sayhentai.me/genre/manhwa", script: "gen.js" },
         { title: "Manga", input: "https://sayhentai.me/genre/manga", script: "gen.js" },
-        { title: "Manhua", input: "https://sayhentai.me/genre/manhua", script: "gen.js" },
         { title: "Hoàn Thành", input: "https://sayhentai.me/completed", script: "gen.js" },
         { title: "Đang theo dõi", input: "https://sayhentai.me/user/bookmark", script: "gen.js" },
     ]);
