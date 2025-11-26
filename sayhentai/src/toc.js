0b0ebb2c6b7250533224c23f2d68079f53b0d6d9
Kizias <quydo144@gmail.com>
2025-11-26 23:35:45 +0700

    update

--- sayhentai/src/toc.js
+++ sayhentai/src/toc.js
@@ -7,7 +7,7 @@ function execute(url) {
         data.push({
             name: e.text(),
             url: e.attr("href"),
-            host: "https://sayhentai.one"
+            host: "https://sayhentai.live"
         })
     }
     return Response.success(data);
